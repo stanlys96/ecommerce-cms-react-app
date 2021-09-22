1f485745a06576debd5b8bcc811be9daf38985e9
Stanly Sukmajaya Kwok <stanlyskwok@gmail.com>
2021-09-23 05:53:27 +0700

    fix bug

--- server/controllers/TransactionHistoryController.js
+++ server/controllers/TransactionHistoryController.js
@@ -3,7 +3,7 @@ const TransactionHistory = require('../models/TransactionHistory');
 class HistoryController {
   static async getHistory(req, res, next) {
     try {
-      const user_id = req.params.userIId;
+      const user_id = req.params.userId;
       const history = await TransactionHistory.getHistory(user_id);
       res.status(200).json(history.rows);
     } catch (err) {
