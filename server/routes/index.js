965ce9cc3339a6522d25a9ed5d1e02d67ad30227
Stanly Sukmajaya Kwok <stanlyskwok@gmail.com>
2021-09-23 05:05:08 +0700

    add history route

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,10 +4,12 @@ const userRoutes = require('./users');
 const productRoutes = require('./products');
 const bannerRoutes = require('./banners');
 const cartRoutes = require('./cart');
+const historyRoutes = require('./history');
 
 router.use('/users', userRoutes);
 router.use('/products', productRoutes);
 router.use('/banners', bannerRoutes);
 router.use('/cart', cartRoutes);
+router.use('/history', historyRoutes);
 
 module.exports = router;
