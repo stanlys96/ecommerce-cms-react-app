24ee6955b0d67eabca06a6ca121e169c2749cc3b
Stanly Sukmajaya Kwok <stanlyskwok@gmail.com>
2021-09-23 04:57:29 +0700

    fix bugs

--- server/routes/history.js
+++ server/routes/history.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const HistoryController = require('../controllers/TransactionHistoryController');
 
-router.get('/getAll/:user_id', HistoryController.getHistory);
+router.get('/getAll/:userId', HistoryController.getHistory);
 router.post('/addToHistory', HistoryController.addToHistory);
 
 module.exports = router;
